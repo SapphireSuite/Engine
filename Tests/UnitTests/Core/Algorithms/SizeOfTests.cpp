340d76e5b4d1c6e613205ed7c822df45c9b71d77
mrouffet <maximerouffet@gmail.com>
2021-05-14 01:20:28 +0200

    [Uncomment] SizeOf 1/2

--- Tests/UnitTests/Core/Algorithms/SizeOfTests.cpp
+++ Tests/UnitTests/Core/Algorithms/SizeOfTests.cpp
@@ -11,18 +11,18 @@ namespace SizeOf_UT
 {
 	void Int()
 	{
-		//const uint32 i = 0;
-		//SA_UTH_EQ(SizeOf(i), 1_ui64);
-		//SA_UTH_EQ(OctSizeOf(i), 4_ui64);
-		//SA_UTH_EQ(BitSizeOf(i), 32_ui64);
+		const uint32 i = 0;
+		SA_UTH_EQ(SizeOf(i), 1_ui64);
+		SA_UTH_EQ(OctSizeOf(i), 4_ui64);
+		SA_UTH_EQ(BitSizeOf(i), 32_ui64);
 	}
 
 	void Float()
 	{
-		//const float f = 0.0f;
-		//SA_UTH_EQ(SizeOf(f), 1_ui64);
-		//SA_UTH_EQ(OctSizeOf(f), 4_ui64);
-		//SA_UTH_EQ(BitSizeOf(f), 32_ui64);
+		const float f = 0.0f;
+		SA_UTH_EQ(SizeOf(f), 1_ui64);
+		SA_UTH_EQ(OctSizeOf(f), 4_ui64);
+		SA_UTH_EQ(BitSizeOf(f), 32_ui64);
 	}
 
 	void Tab()
@@ -36,16 +36,16 @@ namespace SizeOf_UT
 
 	void Vector()
 	{
-		//const std::vector<float> v = { 1.0f, 0.32f, 78.2f, 1.24f, 462.2f };
-		//SA_UTH_EQ(SizeOf(v), v.size());
-		//SA_UTH_EQ(OctSizeOf(v), v.size() * sizeof(float));
-		//SA_UTH_EQ(BitSizeOf(v), v.size() * 32_ui64);
+		const std::vector<float> v = { 1.0f, 0.32f, 78.2f, 1.24f, 462.2f };
+		SA_UTH_EQ(SizeOf(v), v.size());
+		SA_UTH_EQ(OctSizeOf(v), v.size() * sizeof(float));
+		SA_UTH_EQ(BitSizeOf(v), v.size() * 32_ui64);
 	}
 
 	void Args()
 	{
-		//SA_UTH_EQ(SizeOfArgs(), 0_ui64);
-		//SA_UTH_EQ(SizeOfArgs(2, 2.3f, "Hello"), 3_ui64);
+		SA_UTH_EQ(SizeOfArgs(), 0_ui64);
+		SA_UTH_EQ(SizeOfArgs(2, 2.3f, "Hello"), 3_ui64);
 	}
 }
 
