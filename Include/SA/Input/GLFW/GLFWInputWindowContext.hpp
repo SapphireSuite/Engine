d1a2f94e46c3433cc1a5ed204f30c9c656480f71
mrouffet <maximerouffet@gmail.com>
2021-07-12 10:23:55 +0200

    [Fix] missing pragma once

--- Include/SA/Input/GLFW/GLFWInputWindowContext.hpp
+++ Include/SA/Input/GLFW/GLFWInputWindowContext.hpp
@@ -1,5 +1,7 @@
 // Copyright (c) 2021 Sapphire's Suite. All Rights Reserved.
 
+#pragma once
+
 #ifndef SAPPHIRE_INPUT_GLFW_INPUT_WINDOW_CONTEXT_GUARD
 #define SAPPHIRE_INPUT_GLFW_INPUT_WINDOW_CONTEXT_GUARD
 
