61b4b0304565999f7f06c21caeb1a42cacff4661
mrouffet <maximerouffet@gmail.com>
2021-09-27 11:35:34 +0200

    [Rework] Vk single DescriptorSet wrapper v1

--- Include/SA/Render/Vulkan/Pipeline/VkDescriptorSet.hpp
+++ Include/SA/Render/Vulkan/Pipeline/VkDescriptorSet.hpp
@@ -5,33 +5,33 @@
 #ifndef SAPPHIRE_RENDER_VK_DESCRIPTOR_SET_GUARD
 #define SAPPHIRE_RENDER_VK_DESCRIPTOR_SET_GUARD
 
-#include <SA/Render/Base/Pipeline/DescriptorSets/ADescriptorSet.hpp>
+#include <SA/Core/Support/API/Vulkan.hpp>
+#include <SA/Render/Base/Shader/Bindings/ShaderBindingSetDescriptor.hpp>
 
 #if SA_VULKAN
 
 namespace Sa::Vk
 {
 	class Device;
+	class Pipeline;
 
-	class DescriptorSet : public ADescriptorSet
+	class DescriptorSet
 	{
+		VkDescriptorSet mHandle = VK_NULL_HANDLE;
 		VkDescriptorPool mDescriptorPool = VK_NULL_HANDLE;
-		std::vector<VkDescriptorSet> mDescriptorSets;
 
 
-		void CreateDescriptorPool(const Device& _device, const DescriptorSetCreateInfos& _infos);
+		void CreateDescriptorPool(const Device& _device, const ShaderBindingSetDescriptor& _infos);
 		void DestroyDescriptorPool(const Device& _device);
 
 
-		void CreateDescriptorSets(const Device& _device, const DescriptorSetCreateInfos& _infos);
-		void UpdateDescriptorSets(const Device& _device, const std::vector<AShaderBinding*>& _bindings);
-		void DestroyDescriptorSets(const Device& _device);
+		void CreateDescriptorSet(const Device& _device, VkDescriptorSetLayout _descSetLayout);
+		//void UpdateDescriptorSet(const Device& _device, const std::vector<AShaderBinding*>& _bindings);
+		void DestroyDescriptorSet(const Device& _device);
 
 	public:
-		void Create(const Device& _device, const DescriptorSetCreateInfos& _infos);
+		void Create(const Device& _device, const ShaderBindingSetDescriptor& _infos);
 		void Destroy(const Device& _device);
-
-		void Bind(const ARenderFrame& _frame, const ARenderPipeline& _pipeline) const override final;
 	};
 }
 
