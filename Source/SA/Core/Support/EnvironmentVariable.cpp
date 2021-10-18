183de2e39540878455198225294da08723932787
mrouffet <maximerouffet@gmail.com>
2021-10-18 11:15:35 +0200

    [Move] Env Var in Core::

--- Source/SA/Core/Support/EnvironmentVariable.cpp
+++ Source/SA/Core/Support/EnvironmentVariable.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2021 Sapphire's Suite. All Rights Reserved.
 
-#include <SDK/EnvironmentVariable.hpp>
+#include <Core/Support/EnvironmentVariable.hpp>
 
 #include <Collections/Debug>
 
@@ -10,17 +10,10 @@ namespace Sa
 {
 	const std::string EnvironmentVariable::path = []()
 	{
-		//uint64 size = 0u;
-		//char buffer[255]{}; // Max path length.
-
 		std::string envPath = std::getenv("SA_ENGINE_PATH");
 
 		SA_ASSERT(Default, SA/SDK, !envPath.empty(), L"Environment Variable [SA_ENGINE_PATH] not set! Please run the setup script in Engine folder.");
 
 		return envPath;
-
-		//SA_ASSERT_EXEC(Default, SA/SDK, getenv_s(&size, &buffer, "SA_ENGINE_PATH") == 0, L"Environment Variable [SA_ENGINE_PATH] not set! Please run the setup script in Engine folder.");
-
-		//return std::string{ buffer, buffer + size };
 	}();
 }
